bc66e62a9ef86e52d46167c8428e33c35d55bbaa
capeph <capeph@gmail.com>
2024-06-20 16:06:28 +0800

    Adding some tests for setting DoubleValues

--- config/src/main/java/org/capeph/config/DoubleValue.java
+++ config/src/main/java/org/capeph/config/DoubleValue.java
@@ -1,7 +1,7 @@
 package org.capeph.config;
 
 public class DoubleValue extends PathParameter{
-    private double value;
+    private double value = Double.NaN;
 
     public DoubleValue(String path) {
         super(path);
@@ -15,7 +15,7 @@ public class DoubleValue extends PathParameter{
             case Double doubleObj -> doubleObj;
             case Integer intObj -> (double) intObj;
             case String strObj -> Double.parseDouble(strObj);
-            default -> throw new IllegalArgumentException(getPath() + " is not an integer in file config");
+            default -> throw new IllegalArgumentException(getPath() + " is not an double in file config");
         };
     }
 
---
+++ config/src/test/java/org/capeph/config/DoubleValueTest.java
@@ -0,0 +1,25 @@
+package org.capeph.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoubleValueTest {
+
+
+    @Test
+    public void testDoubleValue() {
+        DoubleValue dvn = new DoubleValue("double.value.test.double");
+        assertEquals(Double.NaN, dvn.get());
+        dvn.setValue("12");
+        assertEquals(12.0, dvn.get(), 0.001);
+        dvn.setValue(27.0f);
+        assertEquals(27.0, dvn.get(), 0.001);
+        dvn.setValue(31.5d);
+        assertEquals(31.5, dvn.get(), 0.001);
+        dvn.setValue(17);
+        assertEquals(17.0, dvn.get(), 0.001);
+
+    }
+
+}
